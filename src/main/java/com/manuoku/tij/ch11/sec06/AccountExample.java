586cb6a487c3d3d19e9ecafcf6cb668b0461d147
Jonghwa <dvlpsjh@gmail.com>
2024-02-14 21:25:57 +0900

    study: User-defined Exception

---
+++ src/main/java/com/manuoku/tij/ch11/sec06/AccountExample.java
@@ -0,0 +1,19 @@
+package com.manuoku.tij.ch11.sec06;
+
+public class AccountExample {
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.deposit(1000);
+        acc.deposit(2000);
+        acc.deposit(3000);
+
+        System.out.println(acc.getBalance());
+
+        try {
+            acc.withdraw(7000);
+        } catch (InsufficientException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
