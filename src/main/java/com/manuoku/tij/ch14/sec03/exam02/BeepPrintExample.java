157e6d923076e9d30656da81f9f1a4f71360a8f1
Jonghwa <dvlpsjh@gmail.com>
2024-02-16 23:32:16 +0900

    start: ch14. 멀티스레드

---
+++ src/main/java/com/manuoku/tij/ch14/sec03/exam02/BeepPrintExample.java
@@ -0,0 +1,36 @@
+package com.manuoku.tij.ch14.sec03.exam02;
+
+import java.awt.*;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+                for (int i = 0; i < 5; i++) {
+                    toolkit.beep();
+
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+
+        thread.start(); // 작업스레드 실행
+
+        // 아래부터 메인스레드가 실행하는 코드
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
